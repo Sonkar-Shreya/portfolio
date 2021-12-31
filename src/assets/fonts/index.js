14d534e38dd995bb1efaa4035f0917ecb5dde0bb
Shivesh Tiwari <sthandilya0820@gmail.com>
2021-12-31 10:55:36 +0530

    added fonts

---
+++ src/assets/fonts/index.js
@@ -0,0 +1,36 @@
+// // Fredericathegreat -> sketech kind of
+// export { default as Fredericathegreat } from './Fredericka_the_Great/FrederickatheGreat-Regular.ttf'
+
+// // dancing script -> cursive
+// export { default as dancingScriptBold } from './Dancing_Script/static/DancingScript-Bold.ttf'
+// export { default as dancingScriptMedium } from './Dancing_Script/static/DancingScript-Medium.ttf'
+// export { default as dancingScriptRegular } from './Dancing_Script/static/DancingScript-Regular.ttf'
+// export { default as dancingScriptSemiBold } from './Dancing_Script/static/DancingScript-SemiBold.ttf'
+
+// // playfair -> normal usage
+// export { default as playfairBlack } from './Playfair_Display/static/PlayfairDisplay-Black.ttf'
+// export { default as playfairBlackItalic } from './Playfair_Display/static/PlayfairDisplay-BlackItalic.ttf'
+// export { default as playfairBold } from './Playfair_Display/static/PlayfairDisplay-Bold.ttf'
+// export { default as playfairBoldItalic } from './Playfair_Display/static/PlayfairDisplay-BoldItalic.ttf'
+// export { default as playfairExtraBold } from './Playfair_Display/static/PlayfairDisplay-ExtraBold.ttf'
+// export { default as playfairExtraBoldItalic } from './Playfair_Display/static/PlayfairDisplay-ExtraBoldItalic.ttf'
+// export { default as playfairItalic } from './Playfair_Display/static/PlayfairDisplay-Italic.ttf'
+// export { default as playfairMedium } from './Playfair_Display/static/PlayfairDisplay-Medium.ttf'
+// export { default as playfairMediumItalic } from './Playfair_Display/static/PlayfairDisplay-MediumItalic.ttf'
+// export { default as playfairRegular } from './Playfair_Display/static/PlayfairDisplay-Regular.ttf'
+// export { default as playfairSemiBold } from './Playfair_Display/static/PlayfairDisplay-SemiBold.ttf'
+// export { default as playfairSemiBoldItalic } from './Playfair_Display/static/PlayfairDisplay-SemiBoldItalic.ttf'
+
+// // roboto -> normal usage
+// export { default as robotoBlack } from './Roboto/Roboto-Black.ttf'
+// export { default as robotoBlackItalic } from './Roboto/Roboto-BlackItalic.ttf'
+// export { default as robotoBold } from './Roboto/Roboto-Bold.ttf'
+// export { default as robotoBoldItalic } from './Roboto/Roboto-BoldItalic.ttf'
+// export { default as robotoItalic } from './Roboto/Roboto-Italic.ttf'
+// export { default as robotoMedium } from './Roboto/Roboto-Medium.ttf'
+// export { default as robotoMediumItalic } from './Roboto/Roboto-MediumItalic.ttf'
+// export { default as robotoRegular } from './Roboto/Roboto-Regular.ttf'
+// export { default as robotoThin } from './Roboto/Roboto-Thin.ttf'
+// export { default as robotoThinItalic } from './Roboto/Roboto-ThinItalic.ttf'
+// export { default as robotoLight } from './Roboto/Roboto-Light.ttf'
+// export { default as robotoLightItalic } from './Roboto/Roboto-LightItalic.ttf'
