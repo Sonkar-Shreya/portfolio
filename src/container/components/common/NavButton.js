652050fb7a5ea02dae17d6bf3a379d5649719d3b
Shivesh Tiwari <sthandilya0820@gmail.com>
2022-01-07 11:18:17 +0530

    added underline

--- src/container/components/common/NavButton.js
+++ src/container/components/common/NavButton.js
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react';
 
-const className = 'py-1 text-gray-700 block font-Juana hover:text-purple-500 hover:tracking-wider active:text-purple-600 easeInOut duration-300 hoverable'
+const className = 'py-1 text-gray-700 block font-Juana hover:text-purple-500 hover:tracking-wider active:text-purple-600 easeInOut duration-300 link link-underline link-underline-black'
 
 const NavButton = forwardRef(({href, text}, ref) => (
 	<a ref = {ref} href = {'/'+href} className = {className} > {text} </a>
