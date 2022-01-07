61c4673f7c243ddff6eb6c0df3bc087afe0fc86d
Shivesh Tiwari <sthandilya0820@gmail.com>
2022-01-07 23:43:31 +0530

    added arrow icons

---
+++ src/assets/icons/Arrows.js
@@ -0,0 +1,30 @@
+const RightArrow = () => {
+    return (
+        <svg className="w-24 h-16 cursor-pointer my-5 fill-blue-400 hover:fill-blue-500 easeInOut duration-200" version={1.0} xmlns="http://www.w3.org/2000/svg" width="980.000000pt" height="468.000000pt" viewBox="0 0 980.000000 468.000000" preserveAspectRatio="xMidYMid meet">
+            <g transform="translate(0.000000,468.000000) scale(0.100000,-0.100000)" className="" stroke="none">
+                <path d="M7325 4540 l-130 -130 945 -945 945 -945 -4543 0 -4542 0 0 -180 0
+                -180 4543 0 4542 0 -942 -942 c-519 -519 -943 -945 -943 -948 0 -13 260 -263
+                267 -257 4 4 531 531 1170 1170 l1163 1162 -1162 1162 c-640 640 -1168 1163
+                -1173 1163 -6 0 -69 -59 -140 -130z" />
+            </g>
+        </svg>
+    )
+}
+
+const LeftArrow = () => {
+    return (
+        <svg className="w-24 h-16 cursor-pointer my-5 fill-blue-400 hover:fill-blue-500 easeInOut duration-200 rotate-180" version={1.0} xmlns="http://www.w3.org/2000/svg" width="980.000000pt" height="468.000000pt" viewBox="0 0 980.000000 468.000000" preserveAspectRatio="xMidYMid meet">
+            <g transform="translate(0.000000,468.000000) scale(0.100000,-0.100000)" className="" stroke="none">
+                <path d="M7325 4540 l-130 -130 945 -945 945 -945 -4543 0 -4542 0 0 -180 0
+                -180 4543 0 4542 0 -942 -942 c-519 -519 -943 -945 -943 -948 0 -13 260 -263
+                267 -257 4 4 531 531 1170 1170 l1163 1162 -1162 1162 c-640 640 -1168 1163
+                -1173 1163 -6 0 -69 -59 -140 -130z" />
+            </g>
+        </svg>
+    )
+}
+
+export default {
+    RightArrow,
+    LeftArrow
+}
--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,1 +1,2 @@
 export { default as TechIcons } from '../assets/icons/TechIcons'
+export { default as ArrowIcons } from '../assets/icons/Arrows'
