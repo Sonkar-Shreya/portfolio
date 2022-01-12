d6f100d86b5c5e71aa7c36d2f1ff926d3174062e
Shivesh Tiwari <sthandilya0820@gmail.com>
2022-01-12 20:19:16 +0530

    added routing transition

--- src/container/components/landing/FullScreen.js
+++ src/container/components/landing/FullScreen.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import { motion } from 'framer-motion'
 
 const FullScreen = (props) => {
+	const container = {
+		hidden: { opacity: 0 },
+		show: {
+			opacity: 1,
+			transition: {
+				delayChildren: 0.5
+			}
+		}
+	}
+
+	const item = {
+		hidden: { opacity: 0 },
+		show: { opacity: 1 }
+	}
+
+	const FullScreenVariant = {
+		hidden: {
+			scale: 4,
+			opacity: 0,
+		},
+		show: {
+			scale: 1,
+			opacity: 1,
+			staggerChildren: 0.5
+		},
+		exit: {
+			scale: [1, 4, 1],
+            opacity: 0,
+            transition: {
+                duration: 0.7
+            }
+		},
+	}
+
 	return (
-		<div className = 'h-screen snap-y snap-proximity overflow-y-scroll scroll-smooth'>
+		<motion.div
+			variants={FullScreenVariant}
+			initial='hidden'
+			animate='show'
+			exit='exit'
+			transition={{duration: 0.5, delayChildren: '5s'}}
+			className='h-screen snap-y snap-proximity overflow-y-scroll scroll-smooth'
+		>
 			{props.children}
-		</div>
+		</motion.div>
 	)
 }
 
--- src/container/pages/Project.js
+++ src/container/pages/Project.js
@@ -2,22 +2,65 @@ import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import endpoints from '../../utils/API/endpoints'
 import { data } from '../../constants/'
-import { 
-    DescriptionPage, 
-    Snapshots, 
-    Team 
+import {
+    DescriptionPage,
+    Snapshots,
+    Team
 } from '../components'
+import { motion, useTransform, useViewportScroll } from 'framer-motion'
 
 const Project = () => {
     const { project } = useParams()
     const [projectDetails, setProjectDetails] = useState(data.projects[project])
     const color = projectDetails.style.color
     const teamMembers = projectDetails.team
-    
+
+    const projectPageVariant = {
+        hidden: {
+            scale: 4,
+            opacity: 0,
+            height: '100vh',
+        },
+        show: {
+            scale: 1,
+            opacity: 1,
+            height: '100vh',
+        },
+        exit: {
+            scale: [1, 4, 1],
+            opacity: 0,
+            transition: {
+                duration: 0.7
+            }
+        },
+    }
+
+    const [ffLayer, setFfLayer] = useState(0)
+    const { scrollYProgress } = useViewportScroll()
+    const scaleAnim = useTransform(scrollYProgress, [0, 0.7, 1], [1, 1.2, 0.8])
+    const yPosAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 100, 200])
+    const zRotAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 3, 0])
+
+    scrollYProgress.onChange(x => {
+        setFfLayer(x > 0.4 ? -1 : 0)
+    })
+
     return (
         <div className='font-PlayFair'>
             {/* landing screen */}
-            <div className={'h-screen flex flex-col relative items-center justify-center font-PlayFair tracking-wider text-gray-50 bg-' + color + '-500'}>
+            <motion.div
+                variants={projectPageVariant}
+                animate='show'
+                initial='hidden'
+                exit='exit'
+                transition={{ duration: 0.5 }}
+                style={{
+                    scaleX: scaleAnim,
+                    // y: yPosAnim,
+                    // rotateZ: zRotAnim,
+                }}
+
+                className={'h-screen flex flex-col relative items-center justify-center font-PlayFair tracking-wider text-gray-50 bg-' + color + '-500'}>
                 <nav className='absolute top-5 left-5  md:top-10 md:left-10 md:w-24 p-0'>
                     <Link to={endpoints.slash} className='px-2 py-1 rounded-full md:px-3 md:py-2 border text-white font-bold tracking-widest shadow-sm shadow-gray-50 hover:shadow-white hover:shadow-md duration-300 easeInOut' >
                         Back
@@ -25,7 +68,7 @@ const Project = () => {
                 </nav>
                 <p className='text-4xl md:text-9xl font-Juana p-3'>{projectDetails.title}</p>
                 <p className='py-2 md:text-lg text-left'>{projectDetails.appType}</p>
-            </div>
+            </motion.div>
 
             {/* description screen */}
             <DescriptionPage color={color} projectDetails={projectDetails} />
