9fa193225e4986bacccb8a1242059c58a92c2ede
Shivesh Tiwari <sthandilya0820@gmail.com>
2022-02-28 09:54:26 +0530

    added project snapshots

--- src/container/components/projects/Snapshots.js
+++ src/container/components/projects/Snapshots.js
@@ -1,16 +1,28 @@
 import React from "react";
-
-const Snapshots = ({ color = 'gray', projectDetails={} }) => {
+import {
+    TeamCarousel,
+    TeamCarouselItem
+} from "../";
+const Snapshots = ({ color = 'gray', snapshots = [] }) => {
+    console.log('snapshots', snapshots)
     return (
-        <div>
-
-            {/* 
-                snapshots with one line description
-
-                parallax ho to better rahega
-            
-            */}
-            this contains snapshot
+        <div className="md:p-16 p-5 sm:mt-0 min-h-screen">
+            <div className='lg:w-8/12 mx-auto'>
+                <h2 className='text-2xl md:text-5xl font-Juana py-3'> Snapshots </h2>
+            </div>
+            <div className="w-full lg:pt-16">
+                <TeamCarousel width={'7'}>
+                    {
+                        snapshots.map((img, ind = 0) => (
+                            <TeamCarouselItem key={'image_snapshot_' + ind} width='1000' height={96} >
+                                <div className={`bg-${color}-200 p-2 rounded-lg flex`} style={{width: '1000px'}}>
+                                    <img className={`rounded-lg`} src={img.image} />
+                                </div>
+                            </TeamCarouselItem>
+                        ))
+                    }
+                </TeamCarousel>
+            </div>
         </div>
     )
 }
